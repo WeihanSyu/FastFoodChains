11fdd62245873d9832471480f077a3747dff8165
Weihan <weihansyu@gmail.com>
2025-09-20 20:41:30 -0700

    Added functions and procedures that will be used to clean our location and menu tables

--- Sql_scripts/tbl_setup.sql
+++ Sql_scripts/tbl_setup.sql
@@ -1,7 +1,7 @@
 USE FastFood;
 GO
 
--- Create a lookup table for states, provinces, territories and their abbreviations
+-- Create a lookup table for states, provinces, territories and their abbreviations -----------------------
 DROP TABLE IF EXISTS lookup_abbr;
 GO
 CREATE TABLE lookup_abbr (
@@ -82,10 +82,11 @@ INSERT INTO lookup_abbr VALUES
 ('Nunavut', 'NU');
 GO
 
--- Create tables for our fast food chains
-DROP TABLE IF EXISTS mcdonalds;
+
+-- Create tables for our fast food chains ------------------------------------------------------------------
+DROP TABLE IF EXISTS mcdonalds_location;
 GO 
-CREATE TABLE mcdonalds (
+CREATE TABLE mcdonalds_location (
 	store_id INT IDENTITY(1,1) PRIMARY KEY,
 	country NVARCHAR(20) COLLATE Latin1_General_100_CI_AI_WS_SC_UTF8,
 	state_province NVARCHAR(50) COLLATE Latin1_General_100_CI_AI_WS_SC_UTF8, 
@@ -97,9 +98,9 @@ CREATE TABLE mcdonalds (
 );
 GO
 
-DROP TABLE IF EXISTS wendys;
+DROP TABLE IF EXISTS wendys_location;
 GO
-CREATE TABLE wendys (
+CREATE TABLE wendys_location (
 	store_id INT IDENTITY(1,1) PRIMARY KEY,
 	country NVARCHAR(20) COLLATE Latin1_General_100_CI_AI_WS_SC_UTF8,
 	state_province NVARCHAR(50) COLLATE Latin1_General_100_CI_AI_WS_SC_UTF8,
@@ -111,9 +112,9 @@ CREATE TABLE wendys (
 );
 GO
 
-DROP TABLE IF EXISTS subway;
+DROP TABLE IF EXISTS subway_location;
 GO
-CREATE TABLE subway (
+CREATE TABLE subway_location (
 	store_id INT IDENTITY(1,1) PRIMARY KEY,
 	country NVARCHAR(20) COLLATE Latin1_General_100_CI_AI_WS_SC_UTF8,
 	state_province NVARCHAR(50) COLLATE Latin1_General_100_CI_AI_WS_SC_UTF8,
@@ -125,9 +126,9 @@ CREATE TABLE subway (
 );
 GO
 
-DROP TABLE IF EXISTS kfc;
+DROP TABLE IF EXISTS kfc_location;
 GO
-CREATE TABLE kfc (
+CREATE TABLE kfc_location (
 	store_id INT IDENTITY(1,1) PRIMARY KEY,
 	country NVARCHAR(20) COLLATE Latin1_General_100_CI_AI_WS_SC_UTF8,
 	state_province NVARCHAR(50) COLLATE Latin1_General_100_CI_AI_WS_SC_UTF8,
@@ -139,9 +140,9 @@ CREATE TABLE kfc (
 );
 GO
 
-DROP TABLE IF EXISTS timhortons;
+DROP TABLE IF EXISTS timhortons_location;
 GO
-CREATE TABLE timhortons (
+CREATE TABLE timhortons_location (
 	store_id INT IDENTITY(1,1) PRIMARY KEY,
 	country NVARCHAR(20) COLLATE Latin1_General_100_CI_AI_WS_SC_UTF8,
 	state_province NVARCHAR(50) COLLATE Latin1_General_100_CI_AI_WS_SC_UTF8,
@@ -152,4 +153,204 @@ CREATE TABLE timhortons (
 	longitude DECIMAL(9,6)
 );
 GO
+
+
+-- Create tables for the Menu items -----------------------------------------------------------------------
+DROP TABLE IF EXISTS mcdonalds_menu_CA;
+GO
+CREATE TABLE mcdonalds_menu_CA (
+	item_name NVARCHAR(100),
+	category NVARCHAR(100),
+	price DECIMAL(5, 2),
+	calories INT
+);
+GO
+
+DROP TABLE IF EXISTS mcdonalds_menu_US;
+GO
+CREATE TABLE mcdonalds_menu_US (
+	item_name NVARCHAR(100),
+	category NVARCHAR(100),
+	price DECIMAL(5, 2),
+	calories INT
+);
+GO
+
+DROP TABLE IF EXISTS wendys_menu_CA;
+GO
+CREATE TABLE wendys_menu_CA (
+	item_name NVARCHAR(100),
+	category NVARCHAR(100),
+	price DECIMAL(5, 2),
+	calories INT
+);
+GO
+
+DROP TABLE IF EXISTS wendys_menu_US;
+GO
+CREATE TABLE wendys_menu_US (
+	item_name NVARCHAR(100),
+	category NVARCHAR(100),
+	price DECIMAL(5, 2),
+	calories INT
+);
+GO
+
+DROP TABLE IF EXISTS subway_menu_CA;
+GO
+CREATE TABLE subway_menu_CA (
+	item_name NVARCHAR(100),
+	category NVARCHAR(100),
+	price DECIMAL(5, 2),
+	calories INT
+);
+GO
+
+DROP TABLE IF EXISTS subway_menu_US;
+GO
+CREATE TABLE subway_menu_US (
+	item_name NVARCHAR(100),
+	category NVARCHAR(100),
+	price DECIMAL(5, 2),
+	calories INT
+);
+GO
+
+DROP TABLE IF EXISTS kfc_menu_CA;
+GO
+CREATE TABLE kfc_menu_CA (
+	item_name NVARCHAR(100),
+	category NVARCHAR(100),
+	price DECIMAL(5, 2),
+	calories INT
+);
+GO
+
+DROP TABLE IF EXISTS kfc_menu_US;
+GO
+CREATE TABLE kfc_menu_US (
+	item_name NVARCHAR(100),
+	category NVARCHAR(100),
+	price DECIMAL(5, 2),
+	calories INT
+);
+GO
+
+
+-- Create tables for the calorie lists -----------------------------------------------------------------------
+DROP TABLE IF EXISTS wendys_calories_CA;
+GO
+CREATE TABLE wendys_calories_CA (
+	item_name NVARCHAR(100),
+	calories VARCHAR(15)
+);
+GO
+
+DROP TABLE IF EXISTS wendys_calories_US;
+GO
+CREATE TABLE wendys_calories_US (
+	item_name NVARCHAR(100),
+	calories VARCHAR(15)
+);
+GO
+
+
+-- Create procedures/functions for commonly used cleaning methods (specific to our tables) ------------------
+
+-- Capitalize every first letter in a string (accounting for spaces, and hyphens)
+-- while turning every other character to lowercase
+DROP FUNCTION IF EXISTS dbo.firstCap;
+GO
+CREATE FUNCTION dbo.firstCap (@inputString VARCHAR(4000))
+RETURNS VARCHAR(4000)
+AS
+BEGIN
+	DECLARE @outputString VARCHAR(255);
+	DECLARE @index INT = 1;
+	DECLARE @char CHAR(1);
+	DECLARE @prevChar CHAR(1);
+
+	SET @outputString = LOWER(@inputString);
+
+	WHILE @index <= LEN(@outputString)
+	BEGIN
+		SET @char = SUBSTRING(@outputString, @index, 1);
+		SET @prevChar = CASE WHEN @index = 1 THEN ' '
+							ELSE SUBSTRING(@outputString, @index - 1, 1)
+						END;
+
+		IF @prevChar IN (' ', '-')
+			SET @outputString = STUFF(@outputString, @index, 1, UPPER(@char));
+
+		SET @index += 1;
+	END;
+
+	RETURN @outputString;	
+END;
+GO
+
+-- Return the average calorie for a calorie range in a given table
+DROP PROC IF EXISTS avg_calorie_rng;
+GO
+CREATE PROC avg_calorie_rng
+	@tbl_name NVARCHAR(50)
+AS
+BEGIN
+	-- Check for table existence first. So if someone tries SQL injection, it will not break the proc
+	IF NOT EXISTS (SELECT 1 FROM sys.tables WHERE [name] = @tbl_name) 
+		AND NOT EXISTS (SELECT 1 FROM tempdb.sys.objects WHERE [name] = @tbl_name)
+	BEGIN
+		RAISERROR(N'Invalid input', -1, 1);
+		RETURN;
+	END
+	
+	DECLARE @sql NVARCHAR(4000);
+	SET @sql = 
+		N'WITH CTE AS (
+			SELECT item_name, calories,
+				CAST(LEFT(calories, CHARINDEX(''-'', calories) - 1) AS INT) AS minimum,
+				CAST(RIGHT(calories, LEN(calories) - CHARINDEX(''-'', calories)) AS INT) AS maximum
+			FROM dbo.' + QUOTENAME(@tbl_name) + '
+			WHERE calories LIKE ''%-%''
+		)
+		UPDATE CTE 
+		SET calories = (minimum + maximum) / 2';
+
+	EXEC (@sql);
+END
+GO
+
+-- Return the average calorie for a given calorie split. E.g. '120/200'. Even if there is no 'medium'.
+DROP PROC IF EXISTS avg_calorie_split;
+GO
+CREATE PROC avg_calorie_split
+	@tbl_name NVARCHAR(50)
+AS
+BEGIN
+	-- Check for table existence first. So if someone tries SQL injection, it will not break the proc
+	IF NOT EXISTS (SELECT 1 FROM sys.tables WHERE [name] = @tbl_name) 
+		AND NOT EXISTS (SELECT 1 FROM tempdb.sys.objects WHERE [name] = @tbl_name)
+	BEGIN
+		RAISERROR(N'Invalid input', -1, 1);
+		RETURN;
+	END
+
+	DECLARE @sql NVARCHAR(4000);
+	SET @sql = 
+		N'WITH CTE AS (
+			SELECT item_name, calories,
+				CAST(LEFT(calories, CHARINDEX(''/'', calories) - 1) AS INT) AS minimum,
+				CAST(RIGHT(calories, LEN(calories) - CHARINDEX(''/'', calories)) AS INT) AS maximum
+			FROM dbo.' + QUOTENAME(@tbl_name) + '
+			WHERE calories LIKE ''%/%''
+		)
+		UPDATE CTE 
+		SET calories = (minimum + maximum) / 2';
+
+	EXEC (@sql);
+END
+GO
+
+
+
 
